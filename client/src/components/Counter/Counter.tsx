688b238128e801075a4a5527f2ddcc5a099ddb43
padavanocskos <davidbecsei@gmail.com>
2023-11-24 20:13:43 +0100

    chore(:docker): Add rails to api directory

--- client/src/components/Counter/Counter.tsx
+++ client/src/components/Counter/Counter.tsx
@@ -19,7 +19,7 @@ const Counter: FC<CounterProps> = ({ initialValue }: CounterProps) => {
 
   return (
     <div data-testid="Counter">
-      <p>Initial value is <span id="prime-span" >{ isPrime(initialValue) ? 'a' : 'not'}</span> prime!</p>
+      <p>Initial value is <span id="prime-span">{ isPrime(initialValue) ? 'a' : 'not' }</span> prime!</p>
       <button data-testid="counter-increase" onClick={handleIncrement}>+</button>
       <button data-testid="counter-decrease" onClick={handleDecrement}>-</button>
       <p>Counter Component: <span data-testid="counter-box">{counter}</span></p>
