a451ceca98c36c170b7052ee7d58b5df439eb3c8
padavanocskos <davidbecsei@gmail.com>
2023-12-30 22:30:18 +0100

    chore: Change CRA to Vite

---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
+
+export default defineConfig({
+    // depending on your application, base can also be "/"
+    base: '',
+    plugins: [
+        react(),
+        viteTsconfigPaths(),
+        svgr({
+            include: '**/*.svg?react'
+        })
+    ],
+    server: {    
+        host: true,
+        port: 8000, 
+    },
+})
