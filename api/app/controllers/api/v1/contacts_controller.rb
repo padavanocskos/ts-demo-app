085fbe99f993f30f89dea81d937700239a771ae7
padavanocskos <davidbecsei@gmail.com>
2024-04-16 15:47:16 +0200

    feat(:forms): Add formik form handler with yup

--- api/app/controllers/api/v1/contacts_controller.rb
+++ api/app/controllers/api/v1/contacts_controller.rb
@@ -9,6 +9,23 @@ module Api
         @contacts = @q.result.page(params[:page].to_i + 1).per(params[:per])
         render json: { rows: @contacts, totalRows: @contacts.total_count }
       end
+
+      def create
+        @contact = Contact.new(contact_params)
+
+        if @contact.save
+          render json: @contact, status: :created
+        else
+          render json: @contact.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def contact_params
+        puts 'alma'
+        params.require(:contact).permit(:first_name, :last_name, :middle_name, :phone1, :phone2, :mobile1, :mobile2) # Add the permitted attributes here
+      end
     end
   end
 end
--- react/src/components/Partners/Contacts/ContactForm.tsx
+++ react/src/components/Partners/Contacts/ContactForm.tsx
@@ -1,5 +1,9 @@
 import { Box, Button, Grid, TextField } from "@mui/material";
 import { FC } from "react";
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { useMutation } from "react-query";
+import axios from "axios";
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -13,29 +17,73 @@ const style = {
   p: 4,
 };
 
-function handleSubmit(event) {
-  event.preventDefault();
+// function handleSubmit(event) {
+//   event.preventDefault();
 
-  const fd = new FormData(event.target)
-  const data = Object.fromEntries(fd.entries())
-  console.log("FORM DATA:", data)
-}
+//   const fd = new FormData(event.target)
+//   const data = Object.fromEntries(fd.entries())
+//   console.log("FORM DATA:", data)
+// }
+
+const validationSchema = yup.object({
+  firstName: yup
+    .string()
+    .required('First name is required'),
+  middleName: yup
+    .string()
+    .required('Middle name is required'),
+  lastName: yup
+    .string()
+    .required('Last name is required'),
+  email: yup
+    .string()
+    .email()
+    .required('Email is required')
+
+})
 
 const ContactForm: FC = () => {
+  const mutation = useMutation({
+    mutationFn: (contact) => {
+      let almafa = {}
+      Object.keys(contact).map(key => {
+        let k = key.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
+        almafa[k] = contact[key]
+      })
+      console.log(almafa)
+      return axios.post(`http://localhost:3000/api/v1/contacts`, almafa)
+    }
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      middleName: '',
+      email: '',
+      phone1: '',
+      phone2: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      mutation.mutate({contact: values })
+    }
+  })
+
   return (
-    <Box sx={style} component="form" onSubmit={handleSubmit} >
+    <Box sx={style} component="form" onSubmit={formik.handleSubmit} >
       <Grid container spacing={1}>
         <Grid item xs={12} md={6}>
-          <TextField variant="outlined" label="First name" name='first-name' />
+          <TextField id="firstName" type="text" variant="outlined" label="First name" name='firstName' error={formik.touched.firstName && Boolean(formik.errors.firstName)} value={formik.values.firstName} onChange={formik.handleChange} onBlur={formik.handleBlur} helperText={formik.touched.firstName && formik.errors.firstName} />
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField variant="outlined" label="Middle name" name='middle-name' />
+          <TextField id='middleName' name='middleName' variant="outlined" label="Middle name" error={formik.touched.middleName && Boolean(formik.errors.middleName)} value={formik.values.middleName} onChange={formik.handleChange} onBlur={formik.handleBlur} helperText={formik.touched.middleName && formik.errors.middleName} />
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField variant="outlined" label="Last name" name='last-name' />
+          <TextField id='lastName' variant="outlined" label="Last name" error={formik.touched.lastName && Boolean(formik.errors.lastName)} value={formik.values.lastName} onChange={formik.handleChange} onBlur={formik.handleBlur} name='lastName' helperText={formik.touched.lastName && formik.errors.lastName} />
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField variant="outlined" label="Email" name='email' />
+          <TextField id='email' name='email' variant="outlined" error={formik.touched.email && Boolean(formik.errors.email)} label="Email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} helperText={formik.touched.email && formik.errors.email} />
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField variant="outlined" label="Phone 1" name='phone1' />
